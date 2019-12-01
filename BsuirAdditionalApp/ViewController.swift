74c2a5eb48080ab36a47c9157b01d43af198691a
Kiryl Holubeu <nikto.a.kto@icloud.com>
2019-12-01 17:55:49 +0300

    Initial Commit

---
+++ BsuirAdditionalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BsuirAdditionalApp
+//
+//  Created by Kiryl Holubeu on 12/1/19.
+//  Copyright © 2019 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
