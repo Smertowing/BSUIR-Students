8c7326ccf2cd11425815c1359b5293d36397d612
Kiryl Holubeu <nikto.a.kto@icloud.com>
2019-12-08 18:59:33 +0300

    Menu basic

--- BsuirAdditionalApp/Scenes/Menu/MenuNavigationController.swift
+++ BsuirAdditionalApp/Scenes/Menu/MenuNavigationController.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2019 Kiryl Holubeu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MenuNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storyBoard = UIStoryboard(name: "Menu", bundle: nil)
+        let newsViewController = storyBoard.instantiateViewController(withIdentifier: "menuVC")
+        self.pushViewController(newsViewController, animated: false)
+        let navigationBarAppearace = UINavigationBar.appearance()
+        navigationBarAppearace.tintColor = AppColors.accentColor.uiColor()
+        navigationBarAppearace.barTintColor = AppColors.barsColor.uiColor()
+    }
+
+}
+
--- BsuirAdditionalApp/Scenes/Menu/MenuViewController.swift
+++ BsuirAdditionalApp/Scenes/Menu/MenuViewController.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2019 Kiryl Holubeu. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+    private let viewModel = MenuViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
+        title = "Меню"
+        navigationController?.title = nil
+        navigationController?.navigationBar.isTranslucent = false
+        tabBarController?.tabBar.isTranslucent = false
+        setupViewModel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.isUserInteractionEnabled = true
+
+    }
+
+    private func setupViewModel() {
+        viewModel.delegate = self
+    }
+    
+}
+
+extension MenuViewController: MenuViewModelDelegate {
+    
+}
+
